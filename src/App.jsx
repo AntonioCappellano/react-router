8c16570e171e3d6b4fc095dcf6ac1066d5970763
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-20 15:01:36 +0100

    Add BudgetContext for managing budget state across the application

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,23 @@ import AboutPage from "./pages/AboutPage";
 import DefaultLayout from "./layouts/DefaultLayout";
 import ProductPage from "./pages/ProductPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
+
 import "./assets/products.css";
+import { BudgetProvider } from "./Context/BudgetContext";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route Component={DefaultLayout}>
-          <Route index Component={HomePage} />
-          <Route path="/about-us" Component={AboutPage} />
-          <Route path="/product" Component={ProductPage} />
-          <Route path="/product/:id" Component={ProductDetailPage} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <BudgetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route index Component={HomePage} />
+            <Route path="/about-us" Component={AboutPage} />
+            <Route path="/product" Component={ProductPage} />
+            <Route path="/product/:id" Component={ProductDetailPage} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </BudgetProvider>
   );
 }
